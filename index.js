871269a328b9764d53bac2796bfadf236fe003dc
Kevv07 <141156120+Kevv07@users.noreply.github.com>
2024-08-02 12:09:50 +0100

    Completed project (index.js)
    Completed the tasks assigned as seen in: https://www.freecodecamp.org/learn/back-end-development-and-apis/back-end-development-and-apis-projects/file-metadata-microservice

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
 require('dotenv').config()
 
 var app = express();
@@ -11,8 +12,22 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// START
+const upload = multer({ dest: 'uploads/'})
 
+app.post('/api/fileanalyse', upload.single('upfile'), function(req,res,next){
+  const upFile = req.file;
+  if (!upFile)
+  {
+    return res.json({ error: 'No file uploaded'});
+  }
 
+  return res.json({
+    name: upFile.originalname,
+    type: upFile.mimetype,
+    size: upFile.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
